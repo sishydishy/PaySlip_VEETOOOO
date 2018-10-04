c15f26f456084d4c3f631fd229237b966fc8944a
Saish Dharvotkar <saish.dharvotkar@myob.com>
2018-10-05 10:38:45 +1300

    Created a CSV File Reader and test cases

---
+++ PaySlipVeeToo/PaySlipVeeToo.Test/CSVLoaderTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace PaySlipVeeToo.Test
+{
+    public class CSVLoaderTests
+    {
+        [Fact]
+        public void GivenCSVInputWhenParsingThenReturnEmployeeName()
+        {
+            var csvFileReader = new CSVFileReader();
+            var employeeDetails = csvFileReader.GetEmployeeDetails();
+            var result = employeeDetails[0].FullName;
+            
+            Assert.Equal("David Rudd", result);
+        }
+        
+        [Fact]
+        public void GivenCSVInputWhenParsingThenReturnAnnualSalary()
+        {
+            var csvFileReader = new CSVFileReader();
+            var employeeDetails = csvFileReader.GetEmployeeDetails();
+            var result = employeeDetails[0].AnnualSalary;
+            
+            Assert.Equal(60050, result);
+        }
+        
+        [Fact]
+        public void GivenCSVInputWhenParsingThenReturnSuperRate()
+        {
+            var csvFileReader = new CSVFileReader();
+            var employeeDetails = csvFileReader.GetEmployeeDetails();
+            var result = employeeDetails[0].SuperRate;
+            
+            Assert.Equal("9%", result);
+        }
+        
+        [Fact]
+        public void GivenCSVInputWhenParsingThenReturnPayPeriod()
+        {
+            var csvFileReader = new CSVFileReader();
+            var employeeDetails = csvFileReader.GetEmployeeDetails();
+            var result = employeeDetails[0].PayPeriod;
+            
+            Assert.Equal("01 March – 31 March", result);
+        }
+    }
+}
---
+++ PaySlipVeeToo/PaySlipVeeTwo/CSVFileReader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace PaySlipVeeToo.Test
+{
+    public class CSVFileReader
+    {
+        public List<EmployeeDetails> GetEmployeeDetails()
+        {
+            var filePath =
+                "/Users/saish.dharvotkar/Projects/src/Katas/PaySlip_VEETOOOO/PaySlipVeeToo/PaySlipVeeTwo/Resources/Sample_Input.csv";
+            var streamReader = new StreamReader(filePath);
+            var csv = new CsvReader(streamReader);
+            csv.Configuration.RegisterClassMap<ModelMap>();
+            var records = csv.GetRecords<EmployeeDetails>().ToList();
+
+            return records;
+        }
+        
+        
+    }
+
+    public class ModelMap : ClassMap<EmployeeDetails>
+    {
+        public ModelMap()
+        {
+            Map(x => x.AnnualSalary).Name("annual salary");
+            Map(x => x.FirstName).Name("first name");
+            Map(x => x.LastName).Name("last name");
+            Map(x => x.SuperRate).Name("super rate (%)");
+            Map(x => x.PayPeriod).Name("payment start date");
+        }
+    }
+
+
+    public class EmployeeDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => FirstName + " "+ LastName;
+
+        public decimal AnnualSalary { get; set; }
+        public string SuperRate { get; set; }
+        public string PayPeriod { get; set; }
+    }
+}
